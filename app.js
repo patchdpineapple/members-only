5447774691acba898a1095637f19116748cc0d9c
patchdpineapple <seanpatrick.penaranda@gmail.com>
2021-06-20 21:17:30 +0800

    Signup form now properly creates new user and encrypts password

--- app.js
+++ app.js
@@ -5,11 +5,17 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// setup session and passport
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+// import routers
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var clubRouter = require('./routes/club');
 
-
+// create express app
 var app = express();
 
 // setup mongoose connection
@@ -25,12 +31,59 @@ db.on("error", console.error.bind(console, 'MongoDB connection error:'))
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Use session and passport middleware, define strrategy
+app.use(session({ secret: process.env.SN_SCRT, resave: false, saveUninitialized: true }));
+
+// Define strategy
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username }, (err, user) => {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          // passwords match! log user in
+          return done(null, user);
+        } else {
+          // passwords do not match!
+          return done(null, false, { message: "Incorrect password" });
+        }
+      });
+    });
+  })
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+// Set local variable for user
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
+
+// Routes
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/club', clubRouter);
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,7 @@
 const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const passport = require("passport");
+
 
 // Require models
 var User = require('../models/user');
@@ -7,13 +10,16 @@ const async = require("async");
 
 // Display Home Page
 exports.index = function(req, res) {
-    res.render("index", { title: "Home Page" });
+    res.render("index", { title: "Home Page", user: req.user });
 };
 
 // Post login details
-exports.login_post = function(req, res) {
-    res.redirect("/");
-};
+exports.login_post = [
+    passport.authenticate("local", {
+        successRedirect: "/",
+        failureRedirect: "/"
+      })
+]
 
 // Get signup page
 exports.signup_get = function(req, res) {
@@ -21,7 +27,57 @@ exports.signup_get = function(req, res) {
 };
 
 // Post signup details
-exports.signup_post = function(req, res) {
-    res.render("index", { title: "NOT YET IMPLEMENTED: Sign Up POST" });
-};
+exports.signup_post = [
+    // validation and sanitization
+    body('first_name').trim().isLength({ min: 1 }).escape().withMessage('First name must be specified.')
+        .isAlphanumeric().withMessage('First name has non-alphanumeric characters.'),
+    body('family_name').trim().isLength({ min: 1 }).escape().withMessage('Family name must be specified.')
+        .isAlphanumeric().withMessage('Family name has non-alphanumeric characters.'),
+    body("username").trim().isLength({min:1}).escape().withMessage('First name must be specified.'),
+    body("password", "Password must not be empty").trim().isLength({min:1}).escape(),
+    body("confirm", "Confirm Password must have same value as password field").exists()
+        .custom((value, { req }) => value === req.body.password),
+    body("admin").escape()
+        .customSanitizer( value => {
+            if(value === "") {
+                value = "false";
+            }
+        }),
 
+    // Process request after validation and sanitization.
+    (req, res, next) => {
+        var errors = validationResult(req);
+
+        const user = new User({
+            first_name: req.body.first_name,
+            family_name: req.body.family_name,
+            username: req.body.username,
+            password: req.body.password,
+            admin: req.body.admin
+        });
+
+        if(!errors.isEmpty()) {
+            // There are errors. Render form again with sanitized values/errors messages.
+            res.render('signup_form', { title: 'Signup', user_signup: user, errors: errors.array() });
+            return;
+        } else {
+            bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+                // if err, do something
+                if (err) {
+                  return next(err);
+                }
+            
+                // otherwise, store hashedPassword in DB
+                user.password = hashedPassword;
+                if(req.body.admin === "true") {
+                    user.membership = "Exclusive";
+                }
+                user.save(function(err) {
+                    if(err) { return next(err); }
+                    // Successful - redirect to new author record.
+                    res.redirect("/");
+                });
+              });
+        }
+    }
+]
